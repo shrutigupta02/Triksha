9930576fd1eab44e0a80e7a35e20b16c426c05ca
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2025-10-19 19:50:36 +0530

    Directory structure frontend

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      navbar
+      <div className="logo"></div>
+      <div className="list"></div>
+    </div>
+  );
+}
---
+++ client/src/Pages/Collection.jsx
@@ -0,0 +1,8 @@
+export default function Collection() {
+  return (
+    <div className="collection">
+      collections
+      <div className="heading"></div>
+    </div>
+  );
+}
