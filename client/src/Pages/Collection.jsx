ac4f82012731b9c3cb1a02220cd60a6b36638608
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2025-11-15 12:24:17 +0530

    Added to collection

--- client/src/Pages/Collection.jsx
+++ client/src/Pages/Collection.jsx
@@ -4,9 +4,29 @@ import pink from "../assets/vinni.jpg";
 import tanshui from "../assets/shivi.jpg";
 import yellow from "../assets/lakshita.jpg";
 import tanya from "../assets/tanya.jpg";
+import shivi_night from "../assets/shivi_night.jpg";
+import vini_night from "../assets/vini_night.jpg";
+import shreya from "../assets/white.jpg";
+import brown from "../assets/brown.jpg";
 
 export default function Collection() {
   const products = [
+    {
+      name: "MUSE Bejeweled Fishtail",
+      image: shivi_night,
+    },
+    {
+      name: "MUSE Bejeweled Champagne Set",
+      image: vini_night,
+    },
+    {
+      name: "MUSE White Drape Skirt Set",
+      image: shreya,
+    },
+    {
+      name: "MUSE Brown Drape Skirt Set",
+      image: brown,
+    },
     {
       name: "RANG Pink Lehenga Co-Ord",
       image: pink,
